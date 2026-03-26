6a32ddc4eaca0693b486ff040373447e392d7f28
yaseenkhattak90 <yaseenkhattak11@gmail.com>
2026-03-26 07:21:10 +0000

    container config done

---
+++ 01basics/01variables.js
@@ -0,0 +1,24 @@
+// investigation process //
+
+const accountId = 19956
+let accountEmail = "yaseenkhattak11@gmail.com"
+var acccountPassword = "yaseen123"
+accountCity = "Karachi"
+// accountId = 2//  not allowed
+
+accountEmail = "yaseenkhattak@gmail.com"
+
+accountPassword = "yaseen12345"
+
+accountCity = "Karachi"
+
+let accountState;
+
+console.log(accountId)
+
+/*
+prefer not to use var
+beacuse of issue in block scope and functional scope only use const or let
+*/
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
