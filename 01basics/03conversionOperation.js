737f76a6d6830fd62d437f4f6e79fa0109449ea7
Yaseen Ali Shah <yaseenkhattak11@gmail.com>
2026-03-27 06:00:28 +0000

    container config done

---
+++ 01basics/03conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = "yaseen"
+
+// console.log(typeof (score));
+// console.log(typeof score);
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+   //  Notes Start
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1 ; false => 0
+//
+
+   //  Notes Start
+
+
+
+let isLoggedIn = 1
+
+let booleanLogeedIn = Boolean(isLoggedIn)
+console.log(booleanLogeedIn);
+
