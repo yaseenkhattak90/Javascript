4d7a7331d410764f972fc018f090e778211490ac
Yaseen Ali Shah <yaseenkhattak11@gmail.com>
2026-03-27 07:17:28 +0000

    container config done

--- 01basics/03conversionOperation.js
+++ 01basics/03conversionOperation.js
@@ -43,4 +43,59 @@ let stringNumber = String(someNumber)
 
 let value = 3
 let negValue = -value
-console.log(value);
+// console.log(negValue);
+
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 =" yaseen"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log(1 + 2 + "2");
+// console.log(1 + 2 + "2");
+
+
+
+// console.log(( 3 + 4 ) * 5 % 3);
+
+
+console.log(+true);
+// console.log(+"");  value is 0
+
+// let num1 , num2 , num3 
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+
+let x = 3;
+const y = x++;
+console.log(x);
+console.log(y);
+
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+console.log(y2);
+console.log(x2);
+// x2 is 4n; y2 is 3n
+
+
+
+
