e8eb83ebf014f2ef53a7d27f09404aced9acd4d4
Yaseen Ali Shah <yaseenkhattak11@gmail.com>
2026-03-30 07:34:53 +0000

    container config done

---
+++ 01basics/05datatypes-summary.js
@@ -0,0 +1,41 @@
+// data has two data type 
+
+// primitive
+// 7 types of primitive : string , number , boolean , null , undefined , symbol ,  
+
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outSideTemp = null
+// let userEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+console.log(id === anotherId)
+
+
+
+// const bigNumber = 4362791639323n
+
+// Refrence is called (Nonprimiritive)
+
+
+const heros = ["yaseen , mursaleen ,ali"]
+
+// let myObj{
+//     name: "defaukt",
+//     age = 33,
+// }
+
+const myFunction funnction(){
+    console.log("hello world");
+    
+}
+
+// Array , objects , Functions
+
+console.log(typeof bigNumber);
+
+
